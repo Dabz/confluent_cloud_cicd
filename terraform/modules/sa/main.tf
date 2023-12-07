8a43743f700bf16b70e1be67295e0b0ee2f585a6
Damien Gasparina <d.gasparina@gmail.com>
2023-12-07 15:32:26 +0100

    Adding new modules

---
+++ terraform/modules/sa/main.tf
@@ -0,0 +1,8 @@
+resource "confluent_service_account" "sa" {
+  display_name = var.sa.name
+  description  = "Service account ${var.sa.name} creation"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
